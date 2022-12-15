b9b0186504eb0344a916505620688afb2c4cd464
aayan636 <aayan.kumar123@gmail.com>
2022-12-14 21:32:22 -0800

    minor fixes

--- gnn/data/download_dataset.py
+++ gnn/data/download_dataset.py
@@ -5,7 +5,7 @@ from zipfile import ZipFile
 url = 'https://drive.google.com/file/d/1sAPzKKxLSGvXx83cSSkcfSg2SAD5KDkk/view?usp=share_link'
 filedir = os.path.dirname(os.path.realpath(__file__))
 output = os.path.join(filedir, 'dataset.zip')
-gdown.download(url, output, quiet=False)
+gdown.download(url, output, quiet=False, fuzzy=True)
 
 with ZipFile(output) as zpbj:
     zpbj.extractall(filedir)
